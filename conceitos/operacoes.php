bd0197443f1ca2cdd471d7049f33bd58582c0009
Fabi <108952843+fabirmss@users.noreply.github.com>
2022-09-17 11:51:41 -0400

    Add files via upload
    Execícios realizados no módulo: PHP: conceitos, lidando com dados, loops e mais

---
+++ conceitos/operacoes.php
@@ -0,0 +1,12 @@
+<?php
+
+$soma = 4 + 4;
+$subtracao = 4 - 4;
+$multiplicacao = 4 * 4;
+$divisao = 4 / 4;
+
+$doisAoCubo = 2 ** 3;
+
+$restoDaDivisao = 10 % 3;
+
+echo $restoDaDivisao;
---
+++ conceitos/variavel.php
@@ -0,0 +1,7 @@
+<?php
+
+$idade = 25;
+
+$idade = 21;
+
+echo $idade;
